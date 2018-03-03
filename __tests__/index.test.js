aa6abed4ceb350352b151adc101651eeb74ac7b3
Josh Hawkins <hawkins@users.noreply.github.com>
2018-03-02 23:41:03 -0500

    Add gitdata test and snapshots

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -21,6 +21,49 @@ describe("issues", () => {
       issues: {
         createComment: jest.fn(),
         edit: jest.fn()
+      },
+      pullRequests: {
+        create: jest.fn().mockReturnValue({
+          data: {
+            html_url: "https://example.com/1",
+            number: 4
+          }
+        })
+      },
+      gitdata: {
+        getReference: jest.fn().mockReturnValue({
+          data: {
+            object: {
+              sha: "01"
+            }
+          }
+        }),
+        createBlob: jest.fn().mockReturnValue({
+          data: {
+            sha: "a2",
+            url: "https://example.com/2"
+          }
+        }),
+        getTree: jest.fn().mockReturnValue({
+          data: {
+            sha: "b1"
+          }
+        }),
+        createTree: jest.fn().mockReturnValue({
+          data: {
+            sha: "b2"
+          }
+        }),
+        createCommit: jest.fn().mockReturnValue({
+          data: {
+            sha: "c2"
+          }
+        }),
+        createReference: jest.fn().mockReturnValue({
+          data: {
+            ref: "heads/bot/issue-3"
+          }
+        })
       }
     };
 
@@ -30,17 +73,15 @@ describe("issues", () => {
 
   it("when opened with a new link", async () => {
     await robot.receive(newLinkPayload);
-    expect(github.issues.createComment).toHaveBeenCalled();
+    expect(github.issues.createComment).toMatchSnapshot();
+    expect(github.gitdata.createBlob).toMatchSnapshot();
+    expect(github.gitdata.createCommit).toMatchSnapshot();
+    expect(github.pullRequests.create).toMatchSnapshot();
   });
 
   it("when opened with an existing link", async () => {
     await robot.receive(existingLinkPayload);
-    expect(github.issues.createComment).toHaveBeenCalled();
-    expect(github.issues.edit).toHaveBeenCalledWith({
-      number: 3,
-      owner: "hawkins",
-      repo: "awesome-mobx-bot",
-      state: "closed"
-    });
+    expect(github.issues.createComment).toMatchSnapshot();
+    expect(github.issues.edit).toMatchSnapshot();
   });
 });
