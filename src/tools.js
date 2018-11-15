a7aa80bd25987ffa89ce6d938440d60d01607782
Josh Hawkins <hawkins@users.noreply.github.com>
2018-11-15 12:23:39 -0500

    Improve link positioning regex

--- src/tools.js
+++ src/tools.js
@@ -1,7 +1,7 @@
 module.exports = {
   getSourceIndex: (source, type) => {
     // TODO: Fix this for some sections (i.e., Videos)
-    const format = `^#+ ${type}\\n((\\n^\\*\\s.+)+(\\s+!.+)?)*`;
+    const format = `^#+ ${type}(\\n+^[^#\\n].*$)*`;
     const regex = new RegExp(format, "gmi");
 
     let m = regex.exec(source);
